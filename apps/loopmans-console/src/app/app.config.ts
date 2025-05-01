8e134f284159bc471ba4bf809254fd0fb2d31a2a
AJAYPRASATHGA <ajay@loopmans.in>
2025-05-01 17:10:50 +0530

    prime-theme set-up

--- apps/loopmans-console/src/app/app.config.ts
+++ apps/loopmans-console/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter,withHashLocation } from '@angular/router';
 import { appRoutes } from './app.routes';
-
+import { providePrimeNG } from 'primeng/config';
+import lara from '@primeng/themes/lara'; 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes,withHashLocation()),
+    providePrimeNG({           // add this
+      theme: {                 // add this
+        preset: lara,          // add this
+      },                       // add this
+    }),    
   ],
 };

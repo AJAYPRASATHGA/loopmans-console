d887fdfe8b10136bbe252c38e8aff3692e664ebe
AJAYPRASATHGA <ajay@loopmans.in>
2025-05-01 16:45:08 +0530

    library structure

---
+++ libs/shared/Ui/README.md
@@ -0,0 +1,7 @@
+# Ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test Ui` to execute the unit tests.
---
+++ libs/shared/Ui/src/lib/Ui/Ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui',
+  imports: [CommonModule],
+  templateUrl: './Ui.component.html',
+  styleUrl: './Ui.component.scss',
+})
+export class UiComponent {}

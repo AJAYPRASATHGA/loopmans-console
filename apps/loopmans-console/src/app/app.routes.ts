9ce33b8130bb0ee5d406b2b1a3737fdb22fdba0a
AJAYPRASATHGA <ajay@loopmans.in>
2025-06-01 22:25:27 +0530

    Delete apps directory

--- apps/loopmans-console/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('../../../../libs/auth/src/auth.module').then((m) => m.AuthModule),
-  },
-];
